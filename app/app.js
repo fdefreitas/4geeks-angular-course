112aebe9c83768f65028c3311a51f4df9f521f7e
Fernando De Freitas <fdefreitas6@gmail.com>
2014-10-11 01:50:45 -0430

    initial commit

---
+++ app/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* @ngdoc object
+ * @name 4GeeksAngularCourse
+ * @requires $urlRouterProvider
+ *
+ * @description
+ *
+ */
+angular
+  .module('4GeeksAngularCourse', [
+    'ui.router',
+    'ui.bootstrap',
+    'home'
+  ]);
+
+angular
+  .module('4GeeksAngularCourse')
+  .config(function ($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+  });
---
+++ build.config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  host: 'localhost',
+  port: 3000,
+
+  // app directories
+  appDir: 'app',
+
+  // unit test directories
+  unitTestDir: 'app',
+
+  // build directories
+  buildDir: 'build/',
+  buildComponents: 'build/components/',
+  buildCss: 'build/css/',
+  buildFonts: 'build/fonts/',
+  buildImages: 'build/images/',
+  buildJs: 'build/js/'
+};
+
